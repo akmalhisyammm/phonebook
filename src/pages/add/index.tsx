e787fffc08cb9b2d653a83f892b0d9b5da27e315
Muhammad Akmal Hisyam <muhammad.akmal@student.umn.ac.id>
2023-10-02 03:42:49 +0700

    feat(pages): add next seo in add contact page

--- src/pages/add/index.tsx
+++ src/pages/add/index.tsx
@@ -1,9 +1,17 @@
+import { NextSeo } from 'next-seo';
+
+import { WEB_BASE_URL } from '@/constants/url';
 import { Layout } from '@/components/layouts';
 import { ContactForm } from '@/components/organisms';
 
 const AddContact = () => {
   return (
     <Layout>
+      <NextSeo
+        title="Add Contact"
+        description="Add new contact to your contact list"
+        canonical={`${WEB_BASE_URL}/add`}
+      />
       <ContactForm />
     </Layout>
   );
