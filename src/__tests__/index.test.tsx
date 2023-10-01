6077e932fa6e3f02f2c1df1a17c6ddb80f125d35
Muhammad Akmal Hisyam <muhammad.akmal@student.umn.ac.id>
2023-10-02 03:33:42 +0700

    chore: setup github workflows and unit test

--- src/__tests__/index.test.tsx
+++ src/__tests__/index.test.tsx
@@ -1,11 +1,26 @@
 import { render, screen } from '@testing-library/react';
+import { useRouter } from 'next/router';
 
 import Home from '@/pages';
+import AddContact from '@/pages/add';
 
 import '@testing-library/jest-dom';
 
+jest.mock('next/router', () => ({
+  useRouter: jest.fn(),
+}));
+
+(useRouter as jest.Mock).mockImplementation(() => ({
+  query: { limit: 5, offset: 0, order: 'asc', search: '' },
+}));
+
 test('Render Home page successfully', () => {
   render(<Home />);
+  expect(screen.getByText('Favorites')).toBeInTheDocument();
+  expect(screen.getByText('Regulars')).toBeInTheDocument();
+});
 
-  expect(screen.getByText('Hello, World!')).toBeInTheDocument();
+test('Render Add Contact page successfully', () => {
+  render(<AddContact />);
+  expect(screen.getByText('Add Contact')).toBeInTheDocument();
 });
